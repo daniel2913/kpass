b1ec4a5c718ab2f32076ff51d75adb433d298517
danielzh <daniel29zh@gmail.com>
2024-09-23 18:33:56 +0300

    format

--- dec_stdin.sh
+++ dec_stdin.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-openssl	enc	-d	-aes-256-cbc 			-pbkdf2 -iter $ITER -in $1 			-out ._tmp1	-pass stdin
-openssl	enc	-d 	-aria-256-cbc  		-pbkdf2 -iter $ITER -in ._tmp1 	-out ._tmp2	-pass stdin
-openssl	enc	-d 	-camellia-256-cbc -pbkdf2 -iter $ITER -in ._tmp2	-out $2 		-pass stdin
+openssl enc -d -aes-256-cbc -pbkdf2 -iter $ITER -in $1 -out ._tmp1 -pass stdin
+openssl enc -d -aria-256-cbc -pbkdf2 -iter $ITER -in ._tmp1 -out ._tmp2 -pass stdin
+openssl enc -d -camellia-256-cbc -pbkdf2 -iter $ITER -in ._tmp2 -out $2 -pass stdin
+
 rm ._tmp1 ._tmp2
--- enc.sh
+++ enc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-openssl enc -camellia-256-cbc	-pbkdf2 -iter $ITER -in $1     -out ._tmp1 	-pass pass:$(pass show camellia)
-openssl enc -aria-256-cbc			-pbkdf2 -iter $ITER -in ._tmp1 -out ._tmp2 	-pass pass:$(pass show aria)
-openssl enc -aes-256-cbc			-pbkdf2 -iter $ITER -in ._tmp2 -out $2			-pass pass:$(pass show aes)
+openssl enc -camellia-256-cbc -pbkdf2 -iter $ITER -in $1 -out ._tmp1 -pass pass:$(pass show camellia)
+openssl enc -aria-256-cbc -pbkdf2 -iter $ITER -in ._tmp1 -out ._tmp2 -pass pass:$(pass show aria)
+openssl enc -aes-256-cbc -pbkdf2 -iter $ITER -in ._tmp2 -out $2 -pass pass:$(pass show aes)
+
 rm ._tmp1 ._tmp2
