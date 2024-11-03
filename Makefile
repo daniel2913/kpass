56a3067fc3287941428777a93e96f4a658adf590
daniel2913 <daniel29zh@gmail.com>
2024-11-03 23:48:25 +0300

    update

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
+ITER = 1000000
+export ITER
 update:
-	dotenv -f .env run ./enc.sh db.kdbx db.bin
+	./enc.sh db.kdbx db.bin
 pass_decode:
-	dotenv -f .env run ./dec.sh db.bin db.kdbx
+	./dec.sh db.bin db.kdbx
 stdin_decode:
-	dotenv -f .env run ./dec_stdin.sh db.bin db.kdbx
+	./dec_stdin.sh db.bin db.kdbx
--- dec_stdin.sh
+++ dec_stdin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Enter aes password:" && read -s p && echo $p | openssl enc -d -aes-256-cbc -pbkdf2 -iter $ITER -in $1 -out ._tmp1 -pass stdin &&
-echo "Enter aria password:" && read -s p && ehco $p | openssl enc -d -aria-256-cbc -pbkdf2 -iter $ITER -in ._tmp1 -out ._tmp2 -pass stdin &&
+echo "Enter aria password:" && read -s p && echo $p | openssl enc -d -aria-256-cbc -pbkdf2 -iter $ITER -in ._tmp1 -out ._tmp2 -pass stdin &&
 echo "Enter camellia password:" && read -s p && echo $p | openssl enc -d -camellia-256-cbc -pbkdf2 -iter $ITER -in ._tmp2 -out $2 -pass stdin
 
 rm ._tmp1 ._tmp2
